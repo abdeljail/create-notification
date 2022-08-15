5d99a611534503d856b3153f24f4523655b2f582
abdeljail <taki22abdo@gmail.com>
2022-08-15 18:48:06 +0100

    add functions for elements btn search and close

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@
 import { addClass } from "./helpers/Css/attClass/add.att.js";
 import { hasClass } from "./helpers/Css/attClass/has.att.js";
 import { removeClass } from "./helpers/Css/attClass/remove.att.js";
-import { clickElement, clickElements } from "./helpers/Events/click.e.js";
+import { clickElement, clickElements } from "./helpers/Events/click.event.js";
+import { focusElement } from "./helpers/Events/focus.event.js";
 import { fetchDataString } from "./helpers/apis/get/fetch.string.js";
 import { string } from "./helpers/inputs/inputs.text.js";
 
@@ -31,6 +32,10 @@ const headerShowContentTagName = document.getElementById("haeder-content-show-ta
 const contentHeader = headerShowContentTagName.querySelector("h3");
 
 const inputSreach = headerShowContentTagName.querySelector("input");
+
+const searchBtn = headerShowContentTagName.querySelector("#search-btn");
+
+const closeBtn = headerShowContentTagName.querySelector("#close-btn");
 
 const createElement = responseContentData.firstElementChild.firstElementChild.firstElementChild;
 
@@ -59,7 +64,9 @@ const changeContentTagName = (btn) => {
 };
 
 function createElementDOM(event) {
+
     console.log(event);
+
 }
 
 const DOMelements = (array) => {
@@ -104,11 +111,80 @@ async function addClassBtn(event) {
         return console.log("resault is empty");
     }
 
-    
     responseContentData.lastElementChild.append(...DOMelements(resault));
+
+}
+
+async function serchContent(event) {
+
+    if (inputSreach.value === "")
+        return
+
+    if (hasClass({ element: responseContentData, nameClass: "loading" }))
+        return;
+
+    responseContentData.lastElementChild.innerHTML = ""
+
+    addClass({ el: responseContentData, nameClass: "loading" })
+
+    const { data: { resault }, success } = await fetchDataString({ url: URL_APP + "get/serach" + "/" + inputSreach.name + "/" + inputSreach.value , el: responseContentData, nameClass: "loading" })
+
+    if (!success)
+        return alert("Error getting data from server you are trying to access");
+
+    if (!resault.length) {
+        return console.log("resault is empty");
+    }
+
+    responseContentData.lastElementChild.append(...DOMelements(resault));
+
+}
+
+function emptyInputSearch(event) {
+
+    // if (hasClass({ element: this, nameClass: "active" }))
+    //     return;
+
+    // if (!hasClass({ element: showContentTagName, nameClass: "active" }))
+    //     addClass({ el: showContentTagName, nameClass: "active" })
+
+    // let classBtn = document.querySelector(".btn-aside.active");
+
+    // if (classBtn)
+    //     removeClass({ el: classBtn, nameClass: "active" })
+
+    // addClass({ el: this, nameClass: "active" })
+
+    // changeContentTagName(this)
+    // responseContentData.lastElementChild.innerHTML = ""
+    // const { data: { resault }, success } = await fetchDataString({ url: URL_APP + "get/" + inputSreach.name, element: inputSreach, el: responseContentData, nameClass: "loading" })
+
+    // if (!success)
+    //     return alert("Error getting data from server you are trying to access");
+
+    // if (!resault.length) {
+    //     return console.log("resault is empty");
+    // }
+
+    // responseContentData.lastElementChild.append(...DOMelements(resault));
+
+
+    if (inputSreach.value === "")
+        return;
+
+
+    inputSreach.value = ""
+
 
 
 }
+
+
+
+
+
+
+
 
 const hideContentAside = (event) => {
 
@@ -134,3 +210,9 @@ clickElements({ elements: btnsAside, fun: addClassBtn });
 clickElement({ element: hideContentTagName, fun: hideContentAside })
 
 clickElement({ element: createElement, fun: createElementDOM })
+
+clickElement({ element: searchBtn, fun: serchContent });
+
+clickElement({ element: closeBtn, fun: emptyInputSearch });
+
+
