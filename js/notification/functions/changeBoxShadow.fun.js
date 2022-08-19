45bba7af7a48a7e5cf9bf912c919fdc9fb714290
abdeljail <taki22abdo@gmail.com>
2022-08-19 10:42:39 +0100

    add functions spreadShadow() , boxShadowElement() , blurShadow() , colorShadow() ,showShadowElement()

--- js/notification/functions/changeBoxShadow.fun.js
+++ js/notification/functions/changeBoxShadow.fun.js
@@ -1,15 +1,24 @@
+/**
+ * imports the files helpers from folder helpers and folder functions 
+ */
 
+import { focusElement } from "../../helpers/Events/focus.event.js";
+import { hasClass } from "../../helpers/Css/attClass/has.att.js";
+import { addClass } from "../../helpers/Css/attClass/add.att.js";
+import { removeClass } from "../../helpers/Css/attClass/remove.att.js";
 
-
-
-
-
-
-
+let shadowElement;
 let canvas;
 
 let shadow = [0, 0, 0, 0, "rgba(0, 0, 0, 0.75)"]
 
+/**
+ * change box shadow width element 
+ */
+const boxShadowElement = (element, show) => {
+    shadowElement = show;
+    focusElement({ element: element, fun: showShadowElement })
+}
 /**
  * change Horizontal box Shadow for change value input range
  *  */
@@ -26,7 +35,7 @@ const horizontalShadow = (element, canvasElement) => {
 }
 /**
  * change Vertical box Shadow for change value input range
- *  */
+ **/
 const verticalShadow = (element, canvasElement) => {
 
     canvas = canvasElement;
@@ -38,9 +47,65 @@ const verticalShadow = (element, canvasElement) => {
     })
 
 }
+/**
+ * change Vertical box Shadow for change value input range
+ **/
+const blurShadow = (element, canvasElement) => {
 
+    canvas = canvasElement;
 
+    element.addEventListener('input', () => {
+        element.nextElementSibling.value = element.value;
+        shadow[2] = element.value + "px";
+        canvasElement.style.boxShadow = "  " + shadow[0] + " " + shadow[1] + " " + shadow[2] + " " + shadow[3] + " " + shadow[4] + " "
+    })
+
+}
+/**
+ * change Vertical box Shadow for change value input range
+ *  */
+const colorShadow = (element, canvasElement) => {
+
+    canvas = canvasElement;
+
+    element.addEventListener('input', () => {
+        shadow[4] = element.value;
+        canvasElement.style.boxShadow = "  " + shadow[0] + " " + shadow[1] + " " + shadow[2] + " " + shadow[3] + " " + shadow[4] + " "
+    })
+
+}
+/**
+ * change Spread box Shadow for change value input range
+ *  */
+const spreadShadow = (element, canvasElement) => {
+
+    canvas = canvasElement;
+
+    element.addEventListener('input', () => {
+
+        element.nextElementSibling.value = element.value;
+        shadow[3] = element.value + "px";
+        canvas.style.boxShadow = "  " + shadow[0] + " " + shadow[1] + " " + shadow[2] + " " + shadow[3] + " " + shadow[4] + " "
+        
+    })
+
+}
+/**
+ * show div setting box shadow for click button 
+ */
+function showShadowElement() {
+    if (hasClass({ element: shadowElement, nameClass: "active" }))
+        return;
+    let focus = document.querySelector(".active")
+    if (focus instanceof Element)
+        removeClass({ el: focus, nameClass: "active" });
+    addClass({ el: shadowElement, nameClass: "active" });
+}
 export {
     horizontalShadow,
     verticalShadow,
+    blurShadow,
+    colorShadow,
+    boxShadowElement,
+    spreadShadow
 }
