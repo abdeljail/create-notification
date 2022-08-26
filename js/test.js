fe70917d637c050ce9809cbcb38b825dc244469f
abdeljail <taki22abdo@gmail.com>
2022-08-26 10:53:22 +0100

    test some function for darg drop element in canvas  :rocket: :rotating_light: :label:

--- js/test.js
+++ js/test.js
@@ -1,7 +1,23 @@
-import { Canvas } from "./notification/classes/canvas.js"
+// import { Canvas } from "./notification/classes/canvas.js"
 
-const canvas = new Canvas(document.getElementById("canvas"));
+// const canvas = new Canvas(document.getElementById("canvas"));
 
-console.log(canvas);
-console.log(canvas.loopElements());
-console.log(canvas.canvas);
+// console.log(canvas);
+// console.log(canvas.loopElements());
+// console.log(canvas.canvas);
+
+
+
+// let dragged = null;
+
+
+
+
+// .addEventListener("dragstart", startDrop);
+
+// document.getElementById("canvas").addEventListener("drop", drop);
+
+// document.getElementById("canvas").addEventListener("dragover", dragOver);
+
+
+
